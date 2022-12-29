26ea5cf6f85a08f7e365b2d34a32b99af49eac54
Thiago Souza <thiagorasouza@gmail.com>
2022-12-29 21:02:50 +0000

    feat: ensure that AxiosHttpClient calls axios with correc url

---
+++ src/infra/http/axios/axios-http-client.spec.ts
@@ -0,0 +1,26 @@
+import { AxiosHttpClient } from "./axios-http-client";
+import * as axios from "axios";
+import { faker } from "@faker-js/faker";
+
+jest.mock("axios");
+
+interface SutTypes {
+  sut: AxiosHttpClient;
+}
+
+const makeSut = (): SutTypes => {
+  const sut = new AxiosHttpClient();
+  return { sut };
+};
+
+describe("Axios HTTP Client", () => {
+  it("should call axios with correct url", async () => {
+    const url = faker.internet.url();
+
+    const { sut } = makeSut();
+
+    await sut.post({ url });
+
+    expect(axios).toHaveBeenCalledWith(url);
+  });
+});
---
+++ src/infra/http/axios/axios-http-client.ts
@@ -0,0 +1,13 @@
+import axios from "axios";
+import {
+  HttpPostClient,
+  HttpPostParams,
+} from "../../../data/protocols/http/http-post-client";
+import { HttpResponse } from "../../../data/protocols/http/http-response";
+
+export class AxiosHttpClient implements HttpPostClient<any, any> {
+  async post(params: HttpPostParams<any>): Promise<HttpResponse<any>> {
+    await axios(params.url);
+    return;
+  }
+}
