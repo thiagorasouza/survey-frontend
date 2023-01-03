0dd4c21f7c96431f5cc5e942b7858cdcc26e9147
Thiago Souza <thiagorasouza@gmail.com>
2023-01-03 19:37:32 +0000

    feat: implement routing with react router

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,6 +1,14 @@
 import React from "react";
 import * as ReactDOM from "react-dom/client";
 import Login from "../presentation/pages/Login";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("main"));
-root.render(<Login />);
+root.render(<RouterProvider router={router} />);
