2384368349deda3d89d7700ef951a73c77fb42a1
Thiago Souza <thiagorasouza@gmail.com>
2023-01-03 23:16:24 +0000

    refactor: move Login tests to describe block

--- src/presentation/pages/Login.spec.tsx
+++ src/presentation/pages/Login.spec.tsx
@@ -8,23 +8,25 @@ import "@testing-library/jest-dom";
 import Login from "./Login";
 
 describe("Login Page Test Suite", () => {
-  it("should have an enabled email input", () => {
-    render(<Login />);
-    expect(screen.getByPlaceholderText(/email/i)).not.toBeDisabled();
-  });
+  describe("Initial state", () => {
+    it("should have an enabled email input", () => {
+      render(<Login />);
+      expect(screen.getByPlaceholderText(/email/i)).not.toBeDisabled();
+    });
 
-  it("should have an enabled password input", () => {
-    render(<Login />);
-    expect(screen.getByPlaceholderText(/password/i)).not.toBeDisabled();
-  });
+    it("should have an enabled password input", () => {
+      render(<Login />);
+      expect(screen.getByPlaceholderText(/password/i)).not.toBeDisabled();
+    });
 
-  it("should have an enabled login button", () => {
-    render(<Login />);
-    expect(screen.getByText(/login/i)).not.toBeDisabled();
-  });
+    it("should have an enabled login button", () => {
+      render(<Login />);
+      expect(screen.getByText(/login/i)).not.toBeDisabled();
+    });
 
-  it("should have an enabled signup button", () => {
-    render(<Login />);
-    expect(screen.getByText(/sign ?up/i)).not.toBeDisabled();
+    it("should have an enabled signup button", () => {
+      render(<Login />);
+      expect(screen.getByText(/sign ?up/i)).not.toBeDisabled();
+    });
   });
 });
