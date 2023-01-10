5dda1703eb5028ce5503f017cbefc79b4c78d886
Thiago Souza <thiagorasouza@gmail.com>
2023-01-10 07:49:04 -0500

    feat: add SaveAccessToken to LoginAction factory

--- src/main/factories/loginActionFactory.ts
+++ src/main/factories/loginActionFactory.ts
@@ -1,4 +1,6 @@
+import { LocalSaveAccessToken } from "../../data/usecases/local-save-access-token";
 import { RemoteAuthentication } from "../../data/usecases/remote-authentication";
+import { LocalStorageAdapter } from "../../infra/cache/local-storage-adapter";
 import { AxiosHttpClient } from "../../infra/http/axios/axios-http-client";
 import { LoginAction } from "../../presentation/action/LoginAction";
 import { makeApiUrl } from "./api-url-factory";
@@ -7,5 +9,7 @@ export const makeLoginAction = (): LoginAction => {
   const loginUrl = makeApiUrl("/login");
   const axiosClient = new AxiosHttpClient();
   const authentication = new RemoteAuthentication(loginUrl, axiosClient);
-  return new LoginAction(authentication);
+  const localStorageAdapter = new LocalStorageAdapter();
+  const saveAccessToken = new LocalSaveAccessToken(localStorageAdapter);
+  return new LoginAction(authentication, saveAccessToken);
 };
