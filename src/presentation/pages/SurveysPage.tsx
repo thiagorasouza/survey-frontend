1c9b268d8f132bf12cb81e25c0f56e996e17bf8b
Thiago Souza <thiagorasouza@gmail.com>
2023-02-12 12:51:59 -0500

    feat: create SuveysPage design

--- src/presentation/pages/SurveysPage.tsx
+++ src/presentation/pages/SurveysPage.tsx
@@ -1,11 +1,73 @@
 import React from "react";
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useNavigate } from "react-router-dom";
+import LogoutIcon from "../components/LogoutIcon";
+import { LoaderResult } from "../loaders/LoaderResult";
+
+import styles from "./SurveysPage.scss";
 
 function SurveysPage() {
-  const loaderData = useLoaderData();
+  const navigate = useNavigate();
+
+  const loaderData = useLoaderData() as LoaderResult;
   console.log("🚀 ~ loaderData", loaderData);
 
-  return <h1>Surveys</h1>;
+  const logout = (): void => {
+    navigate("/logout");
+  };
+
+  const openSurvey = (surveyId: string): void => {
+    navigate(`/surveys/${surveyId}/result`);
+  };
+
+  return (
+    <div className={styles.page}>
+      <div className={styles.header}>
+        <div className={styles.greeting}>
+          <h2>Hello, John</h2>
+        </div>
+        <div className={styles.logout}>
+          <button className={styles.btnLogout} type="button" onClick={logout}>
+            <LogoutIcon />
+          </button>
+        </div>
+      </div>
+      <div className={styles.content}>
+        <div className={styles.banners}>
+          <div className={styles.banner}></div>
+          <div className={styles.banner}></div>
+          <div className={styles.banner}></div>
+        </div>
+        <div className={styles.surveys}>
+          {loaderData.data.map((survey) => {
+            const date = new Date(survey.date).toLocaleDateString("en-us", {
+              year: "numeric",
+              month: "long",
+              day: "numeric",
+            });
+            return (
+              <div
+                key={survey.id}
+                className={styles.survey}
+                onClick={() => openSurvey(survey.id)}
+              >
+                <div className={styles.surveyTitle}>{survey.question}</div>
+                <div className={styles.surveyDate}>{date}</div>
+                <div className={styles.surveyProgress}>
+                  <div className={styles.surveyProgressBar}></div>
+                  <div className={styles.surveyOptions}>
+                    {survey.answers.length} options
+                  </div>
+                  <div className={styles.surveyStatus}>
+                    {survey.didAnswer ? "Complete" : "Not answered"}
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default SurveysPage;
