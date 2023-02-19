a5a7d9ae9e9f60827ddad298b46a61749b25c5d2
Thiago Souza <thiagorasouza@gmail.com>
2023-02-19 13:55:54 -0500

    chore: set api in webpack production configuration

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,4 +3,9 @@ const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
   mode: "production",
+  plugins: [
+    new DefinePlugin({
+      "process.env.API_URL": '"https://the-survey-app-api.up.railway.app/api"',
+    }),
+  ],
 });
