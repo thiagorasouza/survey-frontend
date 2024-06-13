5c1b1d2e0a98614caad580a01a412370ff2ecb7b
Thiago Souza <thiagorasouza@gmail.com>
2024-06-13 01:18:59 -0500

    chore: update api url

--- webpack.prod.js
+++ webpack.prod.js
@@ -6,7 +6,7 @@ module.exports = merge(common, {
   mode: "production",
   plugins: [
     new DefinePlugin({
-      "process.env.API_URL": '"https://the-survey-app-backend.cyclic.app/api"',
+      "process.env.API_URL": '"https://survey-backend-w3vy.onrender.com/api"',
     }),
   ],
 });
